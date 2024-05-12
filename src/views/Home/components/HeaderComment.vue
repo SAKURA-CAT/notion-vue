6b4dc981ab4e748e7c0e1cac5fc90c2fcad62d9f
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-05-12 18:03:43 +0800

    update ui

---
+++ src/views/Home/components/HeaderComment.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex shrink-0 gap-3 w-full">
+    <img :src="avatar" class="w-5 h-5 rounded-full border box-border" />
+    <div class="overflow-hidden w-full">
+      <p class="font-semibold">{{ username }}</p>
+      <p class="mt-0.5 text-gray-800 w-full cursor-text">
+        <slot></slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/**
+ * @description: header的评论组件
+ * @file: HeaderComment.vue
+ * @since: 2024-05-12 17:58:07
+ **/
+
+defineProps({
+  avatar: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
